f556da96f8122a1e0faa6b5635b5278ca0d86300
Dursun BASLI <d.basli@it-students.fr>
2022-04-22 15:35:05 +0200

    finish

---
+++ movie.js
@@ -0,0 +1,26 @@
+function fetchMovie(apiKey = '3f951fde1f94ff23e3aebbd24b292474') {
+   
+    let resultSearch = document.getElementById('resultSearch');
+
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&langua
+    ge=fr-FR&page=1`)
+        .then(response => response.json())
+        .then(movies => {
+            console.log(movies);
+            movies.results.forEach(movie => {
+                const cadre = document.createElement('div')
+                cadre.setAttribute('class','test')
+                resultSearch.appendChild(cadre)
+
+                const image = document.createElement('img');
+                image.src = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2"+movie.poster_path;
+                cadre.appendChild(image);
+                
+                const titre = document.createElement('h2')
+                cadre.appendChild(titre)
+                titre.innerText = movie.title
+                
+            });
+        })
+        .catch(err => console.log(err));
+}
